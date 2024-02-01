e52d0ed3e169e27d7a104cc72ea5a815f5e1824b
Bohdan Babych <babychbohdan0@gmail.com>
2024-02-01 11:21:56 +0100

    7 homework

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,5 @@
+const listEl = document.querySelector(".gallery");
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -27,3 +29,13 @@ const images = [
     alt: 'Zebras on Zebra',
   },
 ];
+const items = images.map((image) => {
+  const itemEl = document.createElement('li');
+  const imageEl = document.createElement('img');
+  imageEl.src = image.url;
+  imageEl.alt = image.alt;
+  imageEl.width = 360;
+  itemEl.appendChild(imageEl);
+  return itemEl;
+});
+listEl.append(...items);
